f657f54a3621aacd7d5418b40cfc4dd2c7407fb6
Clacky <develop@clacky.ai>
2025-04-14 05:19:13 -0400

    Add changelog page to display blog update history

---
+++ app/controllers/changelogs_controller.rb
@@ -0,0 +1,6 @@
+class ChangelogsController < ApplicationController
+  def index
+    # This action displays the changelog page
+    # Future implementation could include fetching changelog entries from a database
+  end
+end
